4e6c6b25b094f40238c1a02b91a1c02501b549b8
Vaibhav Farenjiya Khatik <120475570+vfarenjiya@users.noreply.github.com>
2026-09-10 16:17:56 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const VERSION = 'cliff-walker-v1';
+const FONTS = 'cliff-walker-fonts-v1';
+const CORE = ['./', './index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== VERSION && k !== FONTS).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {                 // cache-first, network fallback
+    e.respondWith(caches.match(req, { ignoreSearch: true }).then(hit =>
+      hit || fetch(req).then(res => {
+        const cp = res.clone();
+        caches.open(VERSION).then(c => c.put(req, cp));
+        return res;
+      }).catch(() => caches.match('./index.html'))));
+    return;
+  }
+  if (/fonts\.(googleapis|gstatic)\.com$/.test(url.hostname)) {   // offline fonts after first run
+    e.respondWith(caches.open(FONTS).then(c =>
+      c.match(req).then(hit => hit || fetch(req, { mode: 'cors' }).then(res => {
+        if (res.ok) c.put(req, res.clone());
+        return res;
+      }))));
+  }
+});
