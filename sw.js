8b74ac8a5638b3f264848a37980d2ed79648c3f0
Vaibhav Farenjiya Khatik <120475570+vfarenjiya@users.noreply.github.com>
2026-09-11 14:51:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'snake-v12';
+const VERSION = 'snake-v13';
 const FONTS = 'cliff-walker-fonts-v1';
 const CORE = ['./', './index.html', './style.css',
   './js/config.js', './js/audio.js', './js/net.js', './js/rl.js', './js/render.js', './js/ui.js',
